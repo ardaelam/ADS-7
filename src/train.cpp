67511f15402f899ba066fd4add44db6d9cf44cef
ardaelam <artem.elesin2007@gmail.com>
2026-05-12 22:11:53 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,57 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include "train.h"
+#include <stdexcept>
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+Train::~Train() {
+    if (!first) return;
+    Car* current = first->next;
+    while (current != first) {
+        Car* toDelete = current;
+        current = current->next;
+        delete toDelete;
+    }
+    delete first;
+}
+
+void Train::addCar(bool light) {
+    Car* newCar = new Car(light);
+    if (!first) {
+        first = newCar;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Car* last = first->prev;
+        last->next = newCar;
+        newCar->prev = last;
+        newCar->next = first;
+        first->prev = newCar;
+    }
+}
+
+int Train::getLength() {
+    if (!first) return 0;
+    countOp = 0;
+    Car* start = first;
+    int length = 0;
+
+    // Простой линейный обход: идём по кругу, пока не вернёмся в start
+    Car* current = start;
+    do {
+        current = current->next;
+        ++countOp;
+        ++length;
+    } while (current != start);
+
+    return length;
+}
+
+int Train::getOpCount() const {
+    return countOp;
+}
+
+void Train::resetOpCount() {
+    countOp = 0;
+}
