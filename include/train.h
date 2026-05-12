5c02ce0f91abdd497ebab58efa56f8b060474db7
ardaelam <artem.elesin2007@gmail.com>
2026-05-12 23:25:19 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,27 +1,26 @@
 // train.h
-#ifndef TRAIN_H
-#define TRAIN_H
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
 
 class Train {
-private:
-    struct Car {
-        bool light;
-        Car* next;
-        Car* prev;
-        Car(bool l) : light(l), next(nullptr), prev(nullptr) {}
-    };
+ private:
+  struct Car {
+    bool light;
+    Car* next;
+    Car* prev;
+    explicit Car(bool l) : light(l), next(nullptr), prev(nullptr) {}
+  };
 
-    Car* first;
-    int countOp;
+  Car* first;
+  int countOp;
 
-public:
-    Train();
-    ~Train();
-
-    void addCar(bool light);
-    int getLength();
-    int getOpCount() const;
-    void resetOpCount();
+ public:
+  Train();
+  ~Train();
+  void addCar(bool light);
+  int getLength();
+  int getOpCount() const;
+  void resetOpCount();
 };
 
-#endif
+#endif  // INCLUDE_TRAIN_H_
