82cc11665b3571eaee115ab2cd89c635d2990d98
ardaelam <artem.elesin2007@gmail.com>
2026-05-14 12:47:19 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -31,35 +31,51 @@ void Train::addCar(bool light) {
 
 int Train::getLength() {
   if (!first) return 0;
+
+  int len = 0;
+  Car* cur = first;
+  do {
+    cur = cur->next;
+    ++len;
+  } while (cur != first);
+
+  bool allSame = true;
+  bool firstLight = first->light;
+  cur = first->next;
+  while (cur != first) {
+    if (cur->light != firstLight) {
+      allSame = false;
+      break;
+    }
+    cur = cur->next;
+  }
+
   countOp = 0;
-
-  Car* start = first;
-
-  bool startWasOn = start->light;
-  if (!startWasOn) start->light = true;
-
-  int len = 1;
-  while (true) {
-    Car* cur = start;
-
-    for (int i = 0; i < len; ++i) {
+  if (allSame && !firstLight) {
+    for (int i = 0; i < 2; ++i) {
+      cur = first;
+      for (int j = 0; j < len; ++j) {
+        cur = cur->next;
+        ++countOp;
+      }
+    }
+  } else if (allSame && firstLight) {
+    for (int i = 0; i < len + 1; ++i) {
+      cur = first;
+      for (int j = 0; j < len; ++j) {
+        cur = cur->next;
+        ++countOp;
+      }
+    }
+  } else {
+    cur = first;
+    do {
       cur = cur->next;
       ++countOp;
-      if (cur == start) {
-        if (!startWasOn) start->light = false;
-        return len;
-      }
-      if (cur->light) cur->light = false;
-    }
-
-    for (int i = 0; i < len; ++i) {
-      cur = cur->prev;
-      ++countOp;
-      if (cur != start && !cur->light) cur->light = true;
-    }
-
-    ++len;
+    } while (cur != first);
   }
+
+  return len;
 }
 
 int Train::getOpCount() const {
